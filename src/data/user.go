2125c068604277d12df3c2affcf44aaf330dc915
mernat <mernat777@gmail.com>
2019-02-22 14:31:13 +0300

    fix: UserByEmail fixed

--- src/data/user.go
+++ src/data/user.go
@@ -1,66 +1,68 @@
 package data
 
 import (
+	_ "math/big"
 	"time"
-	_"math/big"
+	"log"
 )
+
 // User defines a user structure
-type User struct{
-	Id	uint32
-	Uuid	string
-	Name	string
-	Email	string
-	Password	string
-	CreatedAt	time.Time	
+type User struct {
+	Id        uint32
+	Uuid      string
+	Name      string
+	Email     string
+	Password  string
+	CreatedAt time.Time
 }
 
-type Session struct{
-	Id 	uint32
-	Uuid	string
-	Email	string
-	UserId	uint32
-	CreatedAt	time.Time
+type Session struct {
+	Id        uint32
+	Uuid      string
+	Email     string
+	UserId    uint32
+	CreatedAt time.Time
 }
 
 // Create a new session for an existing user
-func (user *User) CreateSession()(session Session, err error){
+func (user *User) CreateSession() (session Session, err error) {
 	statement := "insert into sessions (uuid, email, user_id, created_at) values ($1, $2, $3, $4) returning id, uuid, email, user_id, created_at"
 	stmt, err := Db.Prepare(statement)
-	if err != nil{
-		return
+	if err != nil {
+		log.Println(err)
 	}
 	defer stmt.Close()
-	err = stmt.QueryRow(createUUID(), user.Email, user.Id, time.Now()).Scan(&session.Id,&session.Uuid,&session.Email,&session.CreatedAt)
+	err = stmt.QueryRow(createUUID(), user.Email, user.Id, time.Now()).Scan(&session.Id, &session.Uuid, &session.Email, &session.UserId ,&session.CreatedAt)
 	return
 }
 
 // Session Method Gets the session for an existing user
-func (user *User) Session()(session Session, err error){
+func (user *User) Session() (session Session, err error) {
 	session = Session{}
 	statement := "SELECT id, uuid, email, user_id, created_at FROM sessions WHERE user_id = $1"
-	err = Db.QueryRow(statement,user.Id).Scan(&session.Id,&session.UserId,&session.Email,&session.UserId,&session.CreatedAt)
+	err = Db.QueryRow(statement, user.Id).Scan(&session.Id, &session.UserId, &session.Email, &session.UserId, &session.CreatedAt)
 	return
 }
 
 // Check if session is valid in the db
-func (session *Session) Check()(valid bool, err error){
+func (session *Session) Check() (valid bool, err error) {
 	statement := "SELECT id, uuid, email, user_id, created_at FROM sessions WHERE uuid = $1"
-	err = Db.QueryRow(statement,session.Uuid).Scan(&session.Id,&session.UserId,&session.Email,&session.UserId,&session.CreatedAt)
-	if err!= nil{
+	err = Db.QueryRow(statement, session.Uuid).Scan(&session.Id, &session.UserId, &session.Email, &session.UserId, &session.CreatedAt)
+	if err != nil {
 		valid = false
 		return
 	}
-	if session.Id != 0{
+	if session.Id != 0 {
 		valid = true
 	}
 	return
 }
 
 // DeleteByUUID session from database
-func (session *Session) DeleteByUUID()(err error){
+func (session *Session) DeleteByUUID() (err error) {
 	statement := "delete from sessions where uuid = $1"
 	stmt, err := Db.Prepare(statement)
-	if err != nil{
+	if err != nil {
 		return
 	}
 	defer stmt.Close()
@@ -70,43 +72,44 @@ func (session *Session) DeleteByUUID()(err error){
 }
 
 // Get the user from the session
-func (session *Session) User() (user User, err error){
+func (session *Session) User() (user User, err error) {
 	statement := "SELECT id, uuid, name, email, created_at FROM users WHERE id=$1"
-	err = Db.QueryRow(statement,session.UserId).Scan(&user.Id,&user.Uuid,&user.Name,&user.Email,&user.CreatedAt)
+	err = Db.QueryRow(statement, session.UserId).Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.CreatedAt)
 	return
 }
 
 // Delete all sessions from database
-func SessionDeleteAll()(err error){
+func SessionDeleteAll() (err error) {
 	statement := "delete from sessions"
 	_, err = Db.Exec(statement)
 	return
 }
 
 // Create a new user, save user info into the database
-func (user *User) Create() (err error){
+func (user *User) Create() (err error) {
 	statement := "insert into users (uuid, name, email, password, created_at) values ($1, $2, $3, $4, $5) returning id, uuid, created_at"
 	stmt, err := Db.Prepare(statement)
 
-	if err != nil{
+	if err != nil {
 		return
 	}
 
 	defer stmt.Close()
-	err = stmt.QueryRow(createUUID, user.Name, user.Email, Encrypt(user.Password), time.Now()).Scan(&user.Id,&user.Uuid,&user.CreatedAt)
+	err = stmt.QueryRow(createUUID(), user.Name, user.Email, Encrypt(user.Password), time.Now()).Scan(&user.Id, &user.Uuid, &user.CreatedAt)
 	return
 }
 
 // UserByEmail returns a user using an email
-func UserByEmail(email string)(user User, err error){
-	statement := "SELECT id, uuid, name, email, created_at FROM users WHERE email=$1"
-	err = Db.QueryRow(statement,email).Scan(&user.Id,&user.Uuid,&user.Name,&user.Email,&user.CreatedAt)
+func UserByEmail(email string) (user User, err error) {
+	user = User{}
+	statement := "SELECT id, uuid, name, email, password, created_at FROM users WHERE email=$1"
+	err = Db.QueryRow(statement, email).Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.Password, &user.CreatedAt)
 	return
 }
 
 // UserByUUID returns a user using UUID
-func UserByUUID(uuid string)(user User, err error){
+func UserByUUID(uuid string) (user User, err error) {
 	statement := "SELECT id, uuid, name, email, created_at FROM users WHERE uuid=$1"
-	err = Db.QueryRow(statement,uuid).Scan(&user.Id,&user.Uuid,&user.Name,&user.Email,&user.CreatedAt)
+	err = Db.QueryRow(statement, uuid).Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.CreatedAt)
 	return
 }
